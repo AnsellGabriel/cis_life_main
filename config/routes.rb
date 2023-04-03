de17662e7a6d0e560f7cee631df0677bd412301c
maria_rosas <it@1cisp.coop>
2023-04-03 14:39:55 +0800

    fix root path for authorized user

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-        root 'employees#index', as: :authenticated_root
+        root 'application#root', as: :authenticated_root
     end
   
     unauthenticated do
