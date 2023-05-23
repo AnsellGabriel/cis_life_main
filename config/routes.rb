47c9ebf643bd740af3e6c773045cdd60dc5381e2
Ansell Gabriel Del Ayre <ansellgabrieldelayre@Ansells-MacBook-Pro.local>
2023-05-23 15:20:22 +0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :benefits
+  resources :plans
+  get 'pages/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
