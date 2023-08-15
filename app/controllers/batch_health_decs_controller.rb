23922266c8b0beeab9e270abdbb01e797f6a2da4
Rullian Rong <naillur.gnor@gmail.com>
2023-08-15 09:58:43 +0800

    refactor batch_health_dec controller and model

--- app/controllers/batch_health_decs_controller.rb
+++ app/controllers/batch_health_decs_controller.rb
@@ -1,6 +1,7 @@
 class BatchHealthDecsController < InheritedResources::Base
   before_action :authenticate_user!
   before_action :set_batch, only: %i[new create show]
+  before_action :set_group_remit, only: %i[create]
 
   def show 
     @member = @batch.member_details
@@ -14,81 +15,20 @@ class BatchHealthDecsController < InheritedResources::Base
     @questionaire = HealthDec.where(active: true)
   end
 
-  def create
-    @member = @batch.member_details
-    @group_remit = params[:group_remit_id]
-    @batch_health_dec = @batch
-    @questionaire = HealthDec.where(active: true)
-    # Retrieve the form data from params
-    question_params = params[:question]
-    
-    ActiveRecord::Base.transaction do
-      begin
-        pre_approved_health_dec = true
-        # valid_health_dec_ids = HealthDec.where(valid_answer: true).pluck(:id)
-        # identical = valid_health_dec_ids == question_params.keys.map(&:to_i)
+  def create    
+    result = BatchHealthDec.process_health_declaration(@batch, params[:question])
 
-        # unless identical
-        #   raise ActiveRecord::Rollback, "Please answer all questions"
-        # end
+    if result == true
+      redirect_to health_dec_group_remit_batch_path(@group_remit, @batch), notice: "Health declaration saved!"
+    elsif result.is_a?(String)       
+      flash[:error] = result
 
-        question_params.each do |question_id, question_data|
-          # byebug
-          health_dec = HealthDec.find(question_id)
-          
-          if question_data[:answer].nil? || question_data[:answer].blank?
-            raise ActiveRecord::Rollback, "Please answer all questions"
-          end
-    
-          answer = health_dec.batch_health_decs.build(answer: question_data[:answer], batch_id: @batch.id)
-          
-          if question_data[:answer] != health_dec.valid_answer.to_s
-            pre_approved_health_dec = false
-          end
-
-          answer.save!
-    
-          subquestions = question_data[:subquestion]
-    
-          if subquestions.present? && question_data[:answer] == "true"
-            subquestions.each do |subquestion_id, subquestion_data|
-              health_dec_sub = HealthDecSubquestion.find(subquestion_id)
-              subquestion_answer = subquestion_data[:value]
-    
-              if subquestion_answer.blank?
-                raise ActiveRecord::Rollback, "Answer cannot be blank"
-              end
-    
-              subquestion_answer = health_dec_sub.batch_health_decs.build(answer: subquestion_answer, batch_id: @batch.id)
-              subquestion_answer.save!
-            end
-          end
-        end
-    
-        if pre_approved_health_dec
-          @batch.update!(valid_health_dec: true)
-        end
-
-        ActiveRecord::Base.connection.commit_db_transaction
-        
-        redirect_to health_dec_group_remit_batch_path(@group_remit, @batch), notice: "Health declaration saved!"
-        
-      rescue ActiveRecord::Rollback => e
-        flash[:error] = e.message
-        respond_to do |format|
-          format.html { redirect_to new_group_remit_batch_health_declaration_path(@group_remit, @batch) }
-          format.turbo_stream { render turbo_stream: turbo_stream.update('error_notif', partial: 'layouts/notification') }
-        end
-    
-        return
+      respond_to do |format|
+        format.html { redirect_to new_group_remit_batch_health_declaration_path(@group_remit, @batch) }
+        format.turbo_stream { render turbo_stream: turbo_stream.update('error_notif', partial: 'layouts/notification') }
       end
-
-    
+      
     end
-    
-
-
-    
   end
 
   private
@@ -98,5 +38,9 @@ class BatchHealthDecsController < InheritedResources::Base
 
     def set_batch
       @batch = Batch.find(params[:batch_id])
+    end
+
+    def set_group_remit
+      @group_remit = GroupRemit.find(params[:group_remit_id])
     end
 end
--- app/models/batch_health_dec.rb
+++ app/models/batch_health_dec.rb
@@ -7,4 +7,64 @@ class BatchHealthDec < ApplicationRecord
   
   belongs_to :batch
   belongs_to :answerable, polymorphic: true
+
+
+  def self.process_health_declaration(batch, questions)
+    ActiveRecord::Base.transaction do
+      begin
+        @pre_approved_health_dec = true
+
+        questions.each do |q_id, q_data|
+          health_dec = HealthDec.find(q_id)
+          answer = q_data[:answer]
+
+          check_answer(answer)
+          compare_answer(answer, health_dec)
+
+          health_dec_answer = health_dec.batch_health_decs.build(answer: answer, batch_id: batch.id)
+          health_dec_answer.save!
+
+          sub_questions = q_data[:subquestion]
+    
+          if sub_questions.present? && answer == "true"
+            process_subquestion(batch, sub_questions)
+          end
+
+        end
+    
+        batch.update!(valid_health_dec: true) if @pre_approved_health_dec
+        ActiveRecord::Base.connection.commit_db_transaction
+        return true
+        
+      rescue ActiveRecord::Rollback => e
+        return e.message
+      end
+    end
+  end
+
+  def self.check_answer(answer)
+    if answer.nil? || answer.blank?
+      raise ActiveRecord::Rollback, "Please answer all questions"
+    end
+  end
+
+  def self.compare_answer(answer, health_dec)
+    if answer != health_dec.valid_answer.to_s
+      @pre_approved_health_dec = false
+    end
+  end
+
+  def self.process_subquestion(batch, sub_questions)
+    sub_questions.each do |subq_id, subq_data|
+      health_dec_sub = HealthDecSubquestion.find(subq_id)
+      subquestion_answer = subq_data[:value]
+
+      if subquestion_answer.blank?
+        raise ActiveRecord::Rollback, "Answer cannot be blank"
+      end
+
+      subquestion_answer = health_dec_sub.batch_health_decs.build(answer: subquestion_answer, batch_id: batch.id)
+      subquestion_answer.save!
+    end
+  end
 end
