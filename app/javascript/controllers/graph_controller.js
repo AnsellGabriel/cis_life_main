03a2702fba4489a079149d1030ecc61452bb0f71
snjhyrglr <jayjregalario@gmail.com>
2023-11-17 16:50:14 +0800

    update graphs

--- app/javascript/controllers/graph_controller.js
+++ app/javascript/controllers/graph_controller.js
@@ -3,7 +3,7 @@ import { get } from "@rails/request.js"
 // import { ChartKick } from "ChartKick"
 
 export default class extends Controller {
-  static targets = ["typeSelect"]
+  static targets = ["typeSelect", "selectData", "annualData"]
 
   selectType(e) {
     let type_id = e.target.value;
@@ -19,6 +19,36 @@ export default class extends Controller {
     // get(`/coop_members/${member_id}/find_member?group_remit_id=${gr_id}`, {
     //   responseKind: "turbo-stream"
     // })
+  }
+
+  dataSelect(e) {
+    let select_id = e.target.value
+
+    console.log(select_id)
+
+    get(`/select_charts?data_type=${select_id}`, {
+      responseKind: "turbo-stream"
+    })
+  }
+
+  premAnnual(e) {
+    let select_id = e.target.value
+
+    console.log(select_id)
+
+    get(`/update_prem_annum?data_type=${select_id}`, {
+      responseKind: "turbo-stream"
+    })
+
+  }
+  claimAnnual(e) {
+    let select_id = e.target.value
+
+    console.log(select_id)
+
+    get(`/update_claim_annum?data_type=${select_id}`, {
+      responseKind: "turbo-stream"
+    })
   }
 
 }
