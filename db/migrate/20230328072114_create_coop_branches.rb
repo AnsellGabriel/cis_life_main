41e580f3b54f51bb7565b0522c04cc61ed6e85a0
Rullian Rong <naillur.gnor@gmail.com>
2024-01-17 15:51:01 +0800

    fix coop_branches

--- db/migrate/20230328072114_create_coop_branches.rb
+++ db/migrate/20230328072114_create_coop_branches.rb
@@ -2,10 +2,10 @@ class CreateCoopBranches < ActiveRecord::Migration[7.0]
   def change
     create_table :coop_branches do |t|
       t.string :name
-      # t.string :region
-      # t.string :province
-      # t.string :municipality
-      # t.string :barangay
+      t.string :region
+      t.string :province
+      t.string :municipality
+      t.string :barangay
       t.string :contact_details
       t.references :cooperative, null: false, foreign_key: true
 
