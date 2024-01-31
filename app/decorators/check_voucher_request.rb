d5100333408fa4f7256adac3c212257b09cd76bf
Rullian Rong <naillur.gnor@gmail.com>
2024-01-31 17:07:04 +0800

    [WIP] fixing check voucher request for accounting module

--- app/decorators/check_voucher_request.rb
+++ app/decorators/check_voucher_request.rb
@@ -1,4 +1,4 @@
-class ClaimRequestForPaymentDecorator < ApplicationDecorator
+class CheckVoucherRequestDecorator < ApplicationDecorator
   delegate_all
 
   # Define presentation-specific methods here. Helpers are accessed through
