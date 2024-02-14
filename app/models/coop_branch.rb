cb8039b200c45d857517ce39c55edcbd6275d742
Rullian Rong <naillur.gnor@gmail.com>
2024-02-14 14:06:27 +0800

    add search feature to mis coop branches tab

--- app/models/coop_branch.rb
+++ app/models/coop_branch.rb
@@ -28,4 +28,12 @@ class CoopBranch < ApplicationRecord
   def to_upcase
     name.upcase
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name] + _ransackers.keys
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[cooperative] + _ransackers.keys
+  end
 end
