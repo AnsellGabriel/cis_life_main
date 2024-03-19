bcf8a238217398c7b7adfa0cd7e8d71223404dbb
snjhyrglr <jayjregalario@gmail.com>
2024-03-19 09:04:10 +0800

    fix some bugs in underwriting

--- db/migrate/20240308083807_add_payment_type_to_treasury_cashier_entries.rb
+++ db/migrate/20240308083807_add_payment_type_to_treasury_cashier_entries.rb
@@ -1,6 +1,6 @@
 class AddPaymentTypeToTreasuryCashierEntries < ActiveRecord::Migration[7.0]
   def change
-    remove_column :treasury_cashier_entries, :payment_type, :integer
-    add_reference :treasury_cashier_entries, :treasury_payment_type, null: false, foreign_key: true
+    # remove_column :treasury_cashier_entries, :payment_type, :integer
+    # add_reference :treasury_cashier_entries, :treasury_payment_type, null: false, foreign_key: true
   end
 end
