a3328c904d11cabac43007cf1258149c9a151017
Rullian Rong <naillur.gnor@gmail.com>
2024-05-30 14:07:06 +0800

    add counters for accounting dashboard

--- app/controllers/accounting/dashboard_controller.rb
+++ app/controllers/accounting/dashboard_controller.rb
@@ -2,5 +2,10 @@ class Accounting::DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    request = Accounting::VoucherRequest.where(status: :pending)
+    @for_approvals = Accounting::Check.where(status: :for_approval).order(created_at: :desc)
+    @check_req =request.where(payment_type: :check_voucher).order(created_at: :desc)
+    @da_req =request.where(payment_type: :debit_advice).order(created_at: :desc)
+    @journal_req =request.where(payment_type: :journal_voucher).order(created_at: :desc)
   end
 end
