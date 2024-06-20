9265a13dd9c56dba2aa3a22cf4f4d392332776ff
Rullian Rong <naillur.gnor@gmail.com>
2024-06-20 08:52:29 +0800

    fix mis not encoded ors to only show posted ors

--- app/controllers/mis/dashboard_controller.rb
+++ app/controllers/mis/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Mis::DashboardController < ApplicationController
   def index
     @encoded = GroupRemit.where(mis_entry: true)
     # @not_encoded = Treasury::CashierEntry.all
-    @not_encoded = Treasury::CashierEntry.includes(:agreement).where.not(or_no: GroupRemit.pluck(:official_receipt))
+    @not_encoded = Treasury::CashierEntry.posted.includes(:agreement).where.not(or_no: GroupRemit.pluck(:official_receipt))
     @transmitted = TransmittalOr.joins(:transmittal).where(transmittal: { transmittal_type: :mis })
     # @with_ors = GroupRemit.where.not(official_receipt: nil).where(mis_entry: true)
     @with_ors = GroupRemit.where.not(id: TransmittalOr.with_ors_already).where(mis_entry: true)
@@ -17,7 +17,7 @@ class Mis::DashboardController < ApplicationController
     # @encoded = GroupRemit.where(mis_entry: true)
     # @transmitted = TransmittalOr.joins(:transmittal).where(transmittal: { transmittal_type: :mis })
     # @with_ors = GroupRemit.where.not(official_receipt: nil).where(mis_entry: true)
-    
+
     @ors = case @type
     when "enc"
       GroupRemit.where(mis_entry: true)
@@ -26,19 +26,19 @@ class Mis::DashboardController < ApplicationController
       GroupRemit.where.not(id: TransmittalOr.with_ors_already).where(mis_entry: true)
     when "ne"
       # Treasury::CashierEntry.all
-      Treasury::CashierEntry.includes(:agreement).where.not(or_no: GroupRemit.pluck(:official_receipt))
+      Treasury::CashierEntry.posted.includes(:agreement).where.not(or_no: GroupRemit.pluck(:official_receipt))
     end
 
     if params[:search].present?
       if @type == "ne"
-        @filtered_ors = @ors.where(or_no: params[:search]) 
+        @filtered_ors = @ors.where(or_no: params[:search])
       else
-        @filtered_ors = @ors.where(official_receipt: params[:search]) 
+        @filtered_ors = @ors.where(official_receipt: params[:search])
       end
     else
       @filtered_ors = @ors
     end
-    
+
     @pagy_ors, @filtered_ors = pagy(@filtered_ors, items: 25)
   end
 end
