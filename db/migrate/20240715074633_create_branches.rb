dbb737f7284491db9541d1af85fb7de1381fee6a
snjhyrglr <jayjregalario@gmail.com>
2024-07-16 13:38:00 +0800

    comment branch migration file

--- db/migrate/20240715074633_create_branches.rb
+++ db/migrate/20240715074633_create_branches.rb
@@ -1,14 +1,14 @@
 class CreateBranches < ActiveRecord::Migration[7.0]
-  def change
-    create_table :branches do |t|
-      t.string :code
-      t.string :name
-      t.string :branch_code
-      t.string :approver
-      t.string :position
-      t.string :initials
+  # def change
+  #   create_table :branches do |t|
+  #     t.string :code
+  #     t.string :name
+  #     t.string :branch_code
+  #     t.string :approver
+  #     t.string :position
+  #     t.string :initials
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
