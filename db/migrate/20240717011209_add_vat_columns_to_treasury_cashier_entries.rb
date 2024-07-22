8c64db95f371862f93e4356328a238e7d1669c5f
snjhyrglr <jayjregalario@gmail.com>
2024-07-22 09:30:17 +0800

    migration changes v3

--- db/migrate/20240717011209_add_vat_columns_to_treasury_cashier_entries.rb
+++ db/migrate/20240717011209_add_vat_columns_to_treasury_cashier_entries.rb
@@ -1,6 +1,6 @@
 class AddVatColumnsToTreasuryCashierEntries < ActiveRecord::Migration[7.0]
   def change
-    add_column :treasury_cashier_entries, :vatable_amount, :decimal, precision: 15, scale: 2, default: 0
+    # add_column :treasury_cashier_entries, :vatable_amount, :decimal, precision: 15, scale: 2, default: 0
     change_column :treasury_cashier_entries, :amount, :decimal, default: 0, precision: 15, scale: 2
     change_column :treasury_cashier_entries, :unuse, :decimal, default: 0, precision: 15, scale: 2
     add_column :treasury_cashier_entries, :insurance, :boolean, default: false
