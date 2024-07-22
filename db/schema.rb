9d2fcca23d4e303d2a8411e59389d20c8dd6b053
snjhyrglr <jayjregalario@gmail.com>
2024-07-22 08:50:50 +0800

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2024_07_16_033243) do
-  create_table "accounting_journal_entries", charset: "utf8mb4", force: :cascade do |t|
-=======
 ActiveRecord::Schema[7.0].define(version: 2024_07_17_011209) do
   create_table "accounting_journal_entries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
->>>>>>> 233ac794f3a17a6ef210cf4cc6d1bfad15b736aa
     t.string "journable_type", null: false
     t.bigint "journable_id", null: false
     t.bigint "journal_id", null: false
@@ -26,11 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_17_011209) do
     t.index ["journal_id"], name: "index_accounting_journal_entries_on_journal_id"
   end
 
-<<<<<<< HEAD
-  create_table "accounting_vouchers", charset: "utf8mb4", force: :cascade do |t|
-=======
   create_table "accounting_vouchers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
->>>>>>> 233ac794f3a17a6ef210cf4cc6d1bfad15b736aa
     t.date "date_voucher"
     t.string "voucher"
     t.string "payable_type", null: false
