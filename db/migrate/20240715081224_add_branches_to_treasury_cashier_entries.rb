dd6f553d9757e41be609e481083cc426d6b7f3e4
snjhyrglr <jayjregalario@gmail.com>
2024-07-22 09:28:34 +0800

    migration changes v3

--- db/migrate/20240715081224_add_branches_to_treasury_cashier_entries.rb
+++ db/migrate/20240715081224_add_branches_to_treasury_cashier_entries.rb
@@ -1,10 +1,10 @@
 class AddBranchesToTreasuryCashierEntries < ActiveRecord::Migration[7.0]
   def change
-    # add_reference :treasury_cashier_entries, :branch, foreign_key: true
-    # add_column :treasury_cashier_entries, :unuse, :decimal, precision: 15, scale: 2
-    # add_column :treasury_cashier_entries, :vat_exempt, :decimal, precision: 15, scale: 2
-    # add_column :treasury_cashier_entries, :zero_rated, :decimal, precision: 15, scale: 2
-    # remove_column :accounting_vouchers, :branch, :integer
-    # add_reference :accounting_vouchers, :branch, foreign_key: true
+    add_reference :treasury_cashier_entries, :branch, foreign_key: true
+    add_column :treasury_cashier_entries, :unuse, :decimal, precision: 15, scale: 2
+    add_column :treasury_cashier_entries, :vat_exempt, :decimal, precision: 15, scale: 2
+    add_column :treasury_cashier_entries, :zero_rated, :decimal, precision: 15, scale: 2
+    remove_column :accounting_vouchers, :branch, :integer
+    add_reference :accounting_vouchers, :branch, foreign_key: true
   end
 end
